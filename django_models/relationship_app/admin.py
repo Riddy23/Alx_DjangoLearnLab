19610cac4560aa925d791daa7f04a1f5fa70c474
Riddy23 <redemptionsuwali@gmail.com>
2025-09-15 16:52:44 +0000

    done

--- django_models/relationship_app/admin.py
+++ django_models/relationship_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Book, Library, Librarian, UserProfile
 
-# Register your models here.
+# Customize Book admin
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")
+        search_fields = ("title", "author")
+            list_filter = ("publication_year",)
+
+            # Customize Library admin
+            @admin.register(Library)
+            class LibraryAdmin(admin.ModelAdmin):
+                list_display = ("name", "location")
+                    search_fields = ("name",)
+
+                    # Customize Librarian admin
+                    @admin.register(Librarian)
+                    class LibrarianAdmin(admin.ModelAdmin):
+                        list_display = ("first_name", "last_name", "email")
+                            search_fields = ("first_name", "last_name", "email")
+
+                            # Customize UserProfile admin
+                            @admin.register(UserProfile)
+                            class UserProfileAdmin(admin.ModelAdmin):
+                                list_display = ("user", "role")
+                                    list_filter = ("role",)
+                                        search_fields = ("user__username",)
+                                        
