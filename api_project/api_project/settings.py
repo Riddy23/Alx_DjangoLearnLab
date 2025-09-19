58cbad3e01de298f44b1b0f4e98ba80f7b60df49
Riddy23 <redemptionsuwali@gmail.com>
2025-09-19 15:00:01 +0000

    hyr

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_famework.authtoken",
+    "rest_framework.authtoken",
     "api",
 ]
 
