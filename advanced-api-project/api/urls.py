e1e03b6ca78ce689d9470e5a973a07a1620dddef
Riddy23 <redemptionsuwali@gmail.com>
2025-09-26 13:43:14 +0000

    ggdg

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -14,4 +14,10 @@ from .views import (
         path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),
         path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
         path('books/', BookListView.as_view(), name='book-list'),
+        path("books/", BookListView.as_view(), name="book-list"),                     # List & Create (if ListCreateAPIView used)
+        path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),       # Retrieve
+        path("books/create/", BookCreateView.as_view(), name="book-create"),         # Create (if separate)
+        path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"), # Update
+        path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"), # Delete
+                        
     ]
