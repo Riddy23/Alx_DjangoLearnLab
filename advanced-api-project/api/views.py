6dc5207dd25f74114274b32c2ed64bdd10922b80
Riddy23 <redemptionsuwali@gmail.com>
2025-09-27 18:48:53 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated  # ✅ required by checker
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
